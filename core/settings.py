9d976b7c833a437f2e692ca0cd87ab119be77ce5
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 14:29:54 -0300

    feat: configure alerts

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Auth user model
 AUTH_USER_MODEL = "authentication.User"
+
+
+# Django messages configs
+
+MESSAGE_TAGS = {
+    constants.DEBUG: "info",
+    constants.ERROR: "error",
+    constants.SUCCESS: "success",
+    constants.INFO: "info",
+    constants.WARNING: "warning",
+}
