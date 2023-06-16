ff6ea5906df0cf098f1d49e655ec433190b10b9d
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 15:36:56 -0300

    feat: add login required in EventRegisterView

--- core/apps/events/views.py
+++ core/apps/events/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class EventRegisterView(View):
+    @method_decorator(login_required(login_url="/authentication/login/"))
     def get(self, request):
         return render(request, "eventregister.html")
