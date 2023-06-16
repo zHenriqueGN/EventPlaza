c38bed9442119ec4a822b0766cb713062af3c855
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 11:42:53 -0300

    feat: create register template

--- core/apps/authentication/views.py
+++ core/apps/authentication/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
 
 
 class RegisterView(View):
     def get(self, request):
-        return HttpResponse("Initial Test")
+        return render(request, "register.html")
