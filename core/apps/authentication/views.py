dcf6aca540f619dacec01320aded4de26c01c3f1
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 15:05:39 -0300

    feat: create login view

--- core/apps/authentication/views.py
+++ core/apps/authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import JsonResponse
 from django.views import View
-from .controller import register_user
+from .controller import register_user, login_user
 
 
 class RegisterView(View):
@@ -18,4 +18,18 @@ class RegisterView(View):
         }
         if not register_user(request, data):
             return redirect(reverse("register"))
-        return JsonResponse({"message": "User successfully registered"})
+        return redirect(reverse("login"))
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+    def post(self, request):
+        data = {
+            "username": request.POST.get("username"),
+            "password": request.POST.get("password"),
+        }
+        if not login_user(request, data):
+            return redirect(reverse("login"))
+        return JsonResponse({"message": "User logged in!"})
