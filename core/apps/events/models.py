147ccd67829be4c831972f756aa02778681e7423
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 16:10:37 -0300

    feat: create event model

--- core/apps/events/models.py
+++ core/apps/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to="event_logos")
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration = models.IntegerField()
