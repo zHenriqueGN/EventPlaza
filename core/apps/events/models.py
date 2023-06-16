c91561902500bf4b7a817f4b8c432e80710c0919
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 16:53:59 -0300

    fix: fix event logo field not saving

--- core/apps/events/models.py
+++ core/apps/events/models.py
@@ -7,7 +7,7 @@ from core.apps.authentication.models import User
 class Event(models.Model):
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
     title = models.CharField(max_length=50)
-    logo = models.ImageField(upload_to="event_logos")
+    logo = models.ImageField(upload_to="events_logos")
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
