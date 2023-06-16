74117379cd6703620a19fb2f9999788c09c23323
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-16 13:32:24 -0300

    feat: send register form data to register view

--- core/apps/authentication/views.py
+++ core/apps/authentication/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
 
 
 class RegisterView(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        return JsonResponse(dict(request.POST))
