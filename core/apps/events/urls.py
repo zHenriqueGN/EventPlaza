5183d82b569d15d7c7b6c2a2c8637b7a1f1dfe45
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-18 19:25:22 -0300

    feat: create url to edit a event

--- core/apps/events/urls.py
+++ core/apps/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventRegisterView, EventEditView
+from .views import EventRegisterView, EventEditView, EventEditorView
 
 urlpatterns = [
     path("register/", EventRegisterView.as_view(), name="event_register"),
     path("edit/", EventEditView.as_view(), name="event_edit"),
+    path("edit/<int:id>/", EventEditorView.as_view(), name="event_editor"),
 ]
