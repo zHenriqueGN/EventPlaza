9cb460ff2509be4b85297f720db78e7b1965dcb7
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-18 20:30:35 -0300

    feat: create event delete url

--- core/apps/events/urls.py
+++ core/apps/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EventRegisterView, EventEditView, EventEditorView
+from .views import EventRegisterView, EventEditView, EventEditorView, EventDeleteView
 
 urlpatterns = [
     path("register/", EventRegisterView.as_view(), name="event_register"),
     path("edit/", EventEditView.as_view(), name="event_edit"),
     path("edit/<int:id>/", EventEditorView.as_view(), name="event_editor"),
+    path("delete/<int:id>/", EventDeleteView.as_view(), name="event_delete"),
 ]
