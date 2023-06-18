4a3d98906ed4ac42c7c34871e6b6096966314421
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-18 20:15:55 -0300

    feat: create event edit functions

--- core/apps/events/controller.py
+++ core/apps/events/controller.py
@@ -21,3 +21,30 @@ def event_register(request, data) -> bool:
     ).save()
     add_message(request, constants.SUCCESS, "Event successfully registered!")
     return True
+
+
+def event_edit(request, data) -> bool:
+    id = data.get("id")
+    title = data.get("title")
+    logo = data.get("logo")
+    description = data.get("description")
+    start_date = data.get("start_date")
+    end_date = data.get("end_date")
+    duration = data.get("duration")
+    event = Event.objects.get(id=id)
+    if logo:
+        event = update_logo(event, logo)
+    event.title = title
+    event.description = description
+    event.start_date = start_date
+    event.end_date = end_date
+    event.duration = duration
+    event.save()
+    add_message(request, constants.SUCCESS, "Event successfully edited!")
+    return True
+
+
+def update_logo(event, logo) -> Event:
+    event.logo.delete(False)
+    event.logo.save(str(logo), logo)
+    return event
