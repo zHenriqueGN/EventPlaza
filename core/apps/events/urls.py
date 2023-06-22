bef8454dd2067935c1afe854dbd23007d51854e7
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-21 21:07:46 -0300

    feat: create event access url

--- core/apps/events/urls.py
+++ core/apps/events/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import EventRegisterView, EventEditView, EventEditorView, EventDeleteView
+from .views import (
+    EventAccessView,
+    EventRegisterView,
+    EventEditView,
+    EventEditorView,
+    EventDeleteView,
+)
 
 urlpatterns = [
+    path("access/<int:id>", EventAccessView.as_view(), name="event_access"),
     path("register/", EventRegisterView.as_view(), name="event_register"),
     path("edit/", EventEditView.as_view(), name="event_edit"),
     path("edit/<int:id>/", EventEditorView.as_view(), name="event_editor"),
