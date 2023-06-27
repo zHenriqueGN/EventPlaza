35e77f6f106f71e4236a0312cb445566e6951a7b
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-06-27 17:11:52 -0300

    fix: change debug mode to True

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-g4t9_qjkz*zj=l-1coe0(iep6&xd^i)+)h2vxsmm07w2)h&mf&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
